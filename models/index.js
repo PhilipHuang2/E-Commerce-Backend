e5368326b4d5bb8cea9c70bb91697d695eae5675
Philip Huang <philiphuang2@gmail.com>
2022-11-02 23:28:49 -0700

    Added table relationships

--- models/index.js
+++ models/index.js
@@ -5,15 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-// Category.hasOne(Product, {
-//   foreignKey: 
-// })
-
+Product.belongsTo(Category,{
+  foreignKey: "category_id",
+});
 // Categories have many Products
-
+Category.hasMany(Product,{
+    foreignKey: "category_id",
+    onDelete: "Set NULL",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {through: ProductTag});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
 
 module.exports = {
   Product,
