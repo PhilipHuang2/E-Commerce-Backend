d46eabcae4c64df69031f3b6b76e9a085bff87dc
Philip Huang <philiphuang2@gmail.com>
2022-11-03 17:24:16 -0700

    added Product DELETE Route delete one

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -28,21 +28,27 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+  // create a new tag
   try{
     const tagData = Tag.create(req.body);
     res.status(200).json(tagData);
   } catch(err){
     res.status(500).json(err);
   }
-  // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const tagData = Tag.destroy({ where: {id: req.params.id}});
+    res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
