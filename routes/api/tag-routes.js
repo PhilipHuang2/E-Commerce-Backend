1153a679a352f05f732825cfe328385e5cb3402c
Philip Huang <philiphuang2@gmail.com>
2022-11-03 17:19:42 -0700

    added the Post Tag Routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,11 +23,17 @@ router.get('/:id', async (req, res) => {
       include: [{model:Product}]});
       res.status(200).json(tagData);
   } catch(err){
-    res.status(500).json(err);git 
+    res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try{
+    const tagData = Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch(err){
+    res.status(500).json(err);
+  }
   // create a new tag
 });
 
